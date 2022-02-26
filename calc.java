f4718abacc97f5110a2ca9dbf9362a207bb657c7
Sayali <saylikamble1@gmail.com>
2022-02-26 12:10:46 +0530

    hello

---
+++ calc.java
@@ -0,0 +1,12 @@
+class calc 
+{
+
+   public static void main(String[] args) 
+{
+        
+      int a = 10, b = 35, c;
+      c = a+b;
+
+      System.out.println(" Sum of " +a +" and " +b +"  is " +c);
+   }
+}
